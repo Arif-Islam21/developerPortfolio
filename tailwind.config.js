69e3b445c55f4a88b9f63d80b17348a91d1db8c4
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-10-07 19:16:31 +0600

    color selection done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0D1B2A ",
+        secondary: "#0D1B2A",
+        accent: "#B22222",
+        "accent-content": "#8B0000",
+        "base-100": "#0D1B2A",
+        "base-200": "#A3C1DA",
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
