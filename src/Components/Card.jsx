c560cecaae32583350081be1b15b4226103675dc
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-10-08 22:49:08 +0600

    my skills card added

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,14 +1,15 @@
 import image from "../../src/assets/images/express.jfif";
 import image2 from "../../src/assets/images/bootstrap.png";
+import { FaNodeJs } from "react-icons/fa";
 
-const Card = () => {
+const Card = ({ text, icons }) => {
   return (
-    <div className="card my-4 bg-base-100 shadow-xl">
-      <figure>
-        <img src={image} className="w-full" alt="Shoes" />
+    <div className="card my-4 bg-base-100 hover:bg-text-Color hover:text-black hover:scale-110 transition hover:shadow-xl duration-1000 shadow-xl">
+      <figure className="min-w-64 ">
+        <div className="min-w-60 pt-6  flex justify-center">{icons}</div>
       </figure>
       <div className="card-body items-center">
-        <h2 className="card-title">Shoes!</h2>
+        <h2 className="card-title font-merriWeather">{text}</h2>
       </div>
     </div>
   );
--- src/Pages/Home/MySkills/MySkills.jsx
+++ src/Pages/Home/MySkills/MySkills.jsx
@@ -1,19 +1,59 @@
+import { FaGitAlt, FaGithubSquare, FaNodeJs, FaReact } from "react-icons/fa";
 import Card from "../../../Components/Card";
+import { RiFirebaseLine, RiJavascriptFill } from "react-icons/ri";
+import { FaHtml5 } from "react-icons/fa6";
+import {
+  SiCss3,
+  SiExpress,
+  SiMongodb,
+  SiReactquery,
+  SiReactrouter,
+} from "react-icons/si";
 
 const MySkills = () => {
   return (
-    <div>
+    <div className="my-6">
       <div>
         <h2 className="text-3xl lg:text-5xl font-bold text-center text-text-Color my-6">
           My Skills
         </h2>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center gap-4 lg:px-16">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-items-center gap-4 lg:px-16">
+        <Card text={"HTML"} icons={<FaHtml5 size={72} color="#DD4B25" />} />
+        <Card text={"CSS"} icons={<SiCss3 size={72} color="#0577AA" />} />
+        <Card
+          text={"JavaScript"}
+          icons={<RiJavascriptFill size={72} color="#FFFF00" />}
+        />
+        <Card text={"React js"} icons={<FaReact size={72} color="#00FFFF" />} />
+        <Card text={"Node js"} icons={<FaNodeJs size={72} color="#00FFFF" />} />
+        <Card
+          text={"Express js"}
+          icons={<SiExpress size={72} color="#F7F7F7" />}
+        />
+        <Card
+          text={"MongoDB"}
+          icons={<SiMongodb size={72} color="#419432" />}
+        />
+        <Card
+          text={"Firebase"}
+          icons={<RiFirebaseLine size={72} color="#EE7B16" />}
+        />
+        <Card text={"Git"} icons={<FaGitAlt size={72} color="#D74935" />} />
+        <Card
+          text={"Github"}
+          icons={<FaGithubSquare size={72} color="#000000" />}
+        />
+        <div className="">
+          <Card
+            text={"React Router Dom"}
+            icons={<SiReactrouter size={72} color="#EC404F" />}
+          />
+        </div>
+        <Card
+          text={"Tan Stack Query"}
+          icons={<SiReactquery size={72} color="#EC404F" />}
+        />
       </div>
     </div>
   );
