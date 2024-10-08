7b3ef767ac9d0675924886d4645413ecbbe98005
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-10-08 22:59:47 +0600

    working with portfolio section

--- src/Pages/Home/Portfolio/Portfolio.jsx
+++ src/Pages/Home/Portfolio/Portfolio.jsx
@@ -1,3 +1,5 @@
+import port1 from "../../../../src/assets/images/port1.png";
+
 const Portfolio = () => {
   return (
     <div className="my-8">
@@ -5,6 +7,11 @@ const Portfolio = () => {
         <h2 className="text-3xl lg:text-5xl font-bold text-center text-text-Color my-6">
           Portfolio
         </h2>
+      </div>
+      <div>
+        <div className="relative w-80 h-64 overflow-hidden">
+          <img src={port1} alt="" />
+        </div>
       </div>
     </div>
   );
