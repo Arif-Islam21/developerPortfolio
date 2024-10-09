6f52e15e98cd6e41a4c7948bdd0021fb7bb5e499
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-10-09 19:41:33 +0600

    drawer closed

--- src/Pages/Home/Navbar/Navbar.jsx
+++ src/Pages/Home/Navbar/Navbar.jsx
@@ -1,11 +1,10 @@
-import { Link, NavLink } from "react-router-dom";
 import img from "/portfolio.png";
 
 const Navbar = () => {
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
-        <div className="drawer">
+        <div className="drawer ">
           <input id="my-drawer" type="checkbox" className="drawer-toggle" />
           <div className="drawer-content flex justify-between items-center">
             {/* Page content here */}
@@ -47,43 +46,45 @@ const Navbar = () => {
               </ul>
             </div>
           </div>
-          <div className="drawer-side">
+          {/* <div className="drawer-side lg:hidden">
             <label
               htmlFor="my-drawer"
               aria-label="close sidebar"
               className="drawer-overlay"
             ></label>
-            <ul className="menu custom-ul bg-base-100 text-base-content min-h-full w-80 p-4">
+            <ul
+              aria-label="close sidebar"
+              className="menu custom-ul text-white bg-base-100 min-h-full w-80 p-4"
+            >
               {/* Sidebar content here */}
-              <ul className="menu text-white flex lg:hidden flex-col menu-horizontal px-1">
-                <li>
-                  <a href="#about" className="font-merriWeather">
-                    About
-                  </a>
-                </li>
-                <li>
-                  <a href="#skills" className="font-merriWeather">
-                    Skills
-                  </a>
-                </li>
-                <li>
-                  <a href="#portfolio" className="font-merriWeather">
-                    Portfolio
-                  </a>
-                </li>
-                <li>
-                  <a href="#experience" className="font-merriWeather">
-                    Experience
-                  </a>
-                </li>
-                <li>
-                  <a href="#contact" className="font-merriWeather">
-                    Contact
-                  </a>
-                </li>
-              </ul>
+
+          {/* <li>
+                <a href="#about" className="font-merriWeather">
+                  About
+                </a>
+              </li>
+              <li>
+                <a href="#skills" className="font-merriWeather">
+                  Skills
+                </a>
+              </li>
+              <li>
+                <a href="#portfolio" className="font-merriWeather">
+                  Portfolio
+                </a>
+              </li>
+              <li>
+                <a href="#experience" className="font-merriWeather">
+                  Experience
+                </a>
+              </li>
+              <li>
+                <a href="#contact" className="font-merriWeather">
+                  Contact
+                </a>
+              </li>
             </ul>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
