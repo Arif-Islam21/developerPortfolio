e6061410f5fd5edf72d725e861527ecd75984e4f
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-10-09 21:42:17 +0600

    my skills section animation not applied

--- src/Components/SectionTitle.jsx
+++ src/Components/SectionTitle.jsx
@@ -1,10 +1,16 @@
+import { Typewriter } from "react-simple-typewriter";
+
 const SectionTitle = ({ main, sub }) => {
   return (
     <div>
       <h2 className="text-3xl uppercase lg:text-5xl font-bold text-center text-text-Color mt-6">
         {main}
       </h2>
-      <p className="text-center mb-6 font-merriWeather">{sub}</p>
+      {sub && (
+        <p className="text-center mb-6 font-merriWeather">
+          <Typewriter words={[`${sub}`]} cursor cursorStyle={"_"} loop />
+        </p>
+      )}
     </div>
   );
 };
