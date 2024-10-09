843b02f526c472fd8d4713475265c26b32183511
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-10-09 20:51:11 +0600

    typewritter added

--- src/Pages/Home/Footer/Footer.jsx
+++ src/Pages/Home/Footer/Footer.jsx
@@ -1,17 +1,17 @@
 import { FaArrowAltCircleUp } from "react-icons/fa";
-import Typewriter from "typewriter-effect/dist/core";
+import { Typewriter } from "react-simple-typewriter";
 
 const Footer = () => {
-  const typeWritter = new Typewriter("#typewriter", {
-    strings: ["Hello", "World"],
-    autoStart: true,
-  });
-
   return (
     <footer className="footer relative footer-center bg-base-100 text-base-content p-4">
       <aside className="">
         <h2 className="text-3xl font-merriWeather text-text-Color font-bold">
-          {typeWritter.typeString("Thanks For Scrolling")}
+          <Typewriter
+            words={["Thanks For Scrolling"]}
+            cursor
+            cursorStyle={"_"}
+            loop
+          />
         </h2>
         <a
           className="lg:absolute gap-2 items-center text-white py-2 flex right-0"
