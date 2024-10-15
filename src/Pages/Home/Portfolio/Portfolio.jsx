301aa908e784e889a500a66ecb6358ec5ae33480
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-10-15 19:38:33 +0600

    all websites added

--- src/Pages/Home/Portfolio/Portfolio.jsx
+++ src/Pages/Home/Portfolio/Portfolio.jsx
@@ -2,23 +2,59 @@ import port1 from "../../../../src/assets/images/screencapture-hostelhub-75eaf-w
 import port2 from "../../../../src/assets/images/port2.png";
 import port3 from "../../../../src/assets/images/port3.png";
 import port4 from "../../../../src/assets/images/port4.png";
+import port5 from "../../../assets/images/assignment1.png";
+import port6 from "../../../assets/images/assignment2.png";
+import port7 from "../../../assets/images/assignment3.png";
+import port8 from "../../../assets/images/assignment5.png";
+import port9 from "../../../assets/images/assignment6.png";
+import port10 from "../../../assets/images/assignment7.png";
+import port11 from "../../../assets/images/assignment9.png";
+import port12 from "../../../assets/images/11ManageMeal.png";
 import SectionTitle from "../../../Components/SectionTitle";
 import PortfolioCard from "../../../Components/PortfolioCard";
+import { useState } from "react";
 
 const Portfolio = () => {
+  const [showItem, setShowItem] = useState(6);
+  const port = [
+    port1,
+    port2,
+    port3,
+    port4,
+    port5,
+    port6,
+    port7,
+    port8,
+    port9,
+    port10,
+    port11,
+    port12,
+  ];
+
   return (
     <div className="my-8 bg-base-100 py-8 " id="portfolio">
       <SectionTitle main={"Portfolio"} sub={"Some of my works"} />
-      <div className="lg:mx-16 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        <PortfolioCard img={port1} />
-        <PortfolioCard img={port2} />
-        <PortfolioCard img={port3} />
-        <PortfolioCard img={port4} />
+      <div className="lg:mx-16 grid transition-opacity ease-out delay-700 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {port.slice(0, showItem).map((item, idx) => {
+          return <PortfolioCard key={idx} img={`${item}`} />;
+        })}
       </div>
-      <div className="hidden justify-center my-4">
-        <button className="btn btn-outline border-text-Color font-bold text-text-Color hover:bg-text-Color">
-          Show More
-        </button>
+      <div className="flex justify-center my-6">
+        {showItem === 6 ? (
+          <button
+            onClick={() => setShowItem(port.length)}
+            className="btn btn-outline border-text-Color font-bold text-text-Color hover:bg-text-Color"
+          >
+            Show More
+          </button>
+        ) : (
+          <button
+            onClick={() => setShowItem(6)}
+            className="btn btn-outline border-text-Color font-bold text-text-Color hover:bg-text-Color"
+          >
+            Show Less
+          </button>
+        )}
       </div>
     </div>
   );
