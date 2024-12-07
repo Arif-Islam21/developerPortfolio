bd256b5d97b1abea4c574099467cde8f8b7e643c
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-12-07 23:25:54 +0600

    responsive navbar added

--- src/Pages/Home/Navbar/Navbar.jsx
+++ src/Pages/Home/Navbar/Navbar.jsx
@@ -1,51 +1,99 @@
 import img from "/portfolio.png";
+import img2 from "/portfolio2.jpeg";
 
 const Navbar = () => {
+  const links = (
+    <>
+      <button className="btn btn-sm btn-outline border-text-Color text-text-Color mr-2">
+        <a href="#about" className="font-merriWeather">
+          About
+        </a>
+      </button>
+      <button className="btn btn-sm btn-outline border-text-Color text-text-Color mr-2">
+        <a href="#skills" className="font-merriWeather">
+          Skills
+        </a>
+      </button>
+      <button className="btn btn-sm btn-outline border-text-Color text-text-Color mr-2">
+        <a href="#portfolio" className="font-merriWeather">
+          Portfolio
+        </a>
+      </button>
+      <button className="btn btn-sm btn-outline border-text-Color text-text-Color mr-2">
+        <a href="#experience" className="font-merriWeather">
+          Experience
+        </a>
+      </button>
+    </>
+  );
+
   return (
-    <div className="navbar flex justify-between py-4 items-center bg-base-100">
-      <div
-        data-aos="fade-zoom-in"
-        data-aos-offset="300"
-        data-aos-duration="500"
-        data-aos-easing="ease-in-back"
-        className="ring-lime-200 lg:ml-16  ring-offset-base-100 w-14 rounded-full ring ring-offset-2"
-      >
-        <img src={img} />
+    // <div className="navbar flex justify-between py-4 items-center bg-base-100">
+    //   <div
+    //     data-aos="fade-zoom-in"
+    //     data-aos-offset="300"
+    //     data-aos-duration="500"
+    //     data-aos-easing="ease-in-back"
+    //     className="ring-lime-200 lg:ml-16  ring-offset-base-100 w-14 rounded-full ring ring-offset-2"
+    //   >
+    //     <img src={img} />
+    //   </div>
+    //   <div
+    //     data-aos="fade-zoom-in"
+    //     data-aos-offset="300"
+    //     data-aos-duration="500"
+    //     data-aos-easing="ease-in-back"
+    //     className="flex-none  lg:pr-12"
+    //   >
+    //     <ul className="menu hidden lg:flex menu-horizontal px-1">
+
+    //     </ul>
+    //   </div>
+    // </div>
+    // responsive navbr
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            {links}
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl">
+          <div className="avatar online">
+            <div className="w-12 rounded-full">
+              <img src={img2} />
+            </div>
+          </div>
+        </a>
       </div>
-      <div
-        data-aos="fade-zoom-in"
-        data-aos-offset="300"
-        data-aos-duration="500"
-        data-aos-easing="ease-in-back"
-        className="flex-none  lg:pr-12"
-      >
-        <ul className="menu hidden lg:flex menu-horizontal px-1">
-          <li>
-            <a href="#about" className="font-merriWeather">
-              About
-            </a>
-          </li>
-          <li>
-            <a href="#skills" className="font-merriWeather">
-              Skills
-            </a>
-          </li>
-          <li>
-            <a href="#portfolio" className="font-merriWeather">
-              Portfolio
-            </a>
-          </li>
-          <li>
-            <a href="#experience" className="font-merriWeather">
-              Experience
-            </a>
-          </li>
-          <li>
-            <a href="#contact" className="font-merriWeather">
-              Contact
-            </a>
-          </li>
-        </ul>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{links}</ul>
+      </div>
+      <div className="navbar-end">
+        <button className="btn btn-sm btn-outline border-text-Color text-text-Color">
+          <a href="#contact" className="font-merriWeather">
+            Contact
+          </a>
+        </button>
       </div>
     </div>
   );
