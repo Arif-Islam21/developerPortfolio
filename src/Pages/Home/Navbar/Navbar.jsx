a690ee3b68007a24208079b1ac316f9ff2017ec8
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-12-07 23:28:20 +0600

    navbar done

--- src/Pages/Home/Navbar/Navbar.jsx
+++ src/Pages/Home/Navbar/Navbar.jsx
@@ -30,19 +30,13 @@ const Navbar = () => {
   return (
     // <div className="navbar flex justify-between py-4 items-center bg-base-100">
     //   <div
-    //     data-aos="fade-zoom-in"
-    //     data-aos-offset="300"
-    //     data-aos-duration="500"
-    //     data-aos-easing="ease-in-back"
+
     //     className="ring-lime-200 lg:ml-16  ring-offset-base-100 w-14 rounded-full ring ring-offset-2"
     //   >
     //     <img src={img} />
     //   </div>
     //   <div
-    //     data-aos="fade-zoom-in"
-    //     data-aos-offset="300"
-    //     data-aos-duration="500"
-    //     data-aos-easing="ease-in-back"
+
     //     className="flex-none  lg:pr-12"
     //   >
     //     <ul className="menu hidden lg:flex menu-horizontal px-1">
