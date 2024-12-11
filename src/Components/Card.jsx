b65fb0d6c9d55409cdb0dbc0132a5ad03009d324
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-12-12 00:58:11 +0600

    alas, i cant do this.

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,22 +1,16 @@
-// const Card = ({ text, icons }) => {
-//   return (
-//     <div data-aos="zoom-in" data-aos-duration="1500">
-//       <div className="card my-4 bg-base-100 hover:bg-text-Color hover:text-black hover:scale-110 transition hover:shadow-xl duration-1000 shadow-xl">
-// <figure className="min-w-64 ">
-//   <div className="min-w-60 pt-6  flex justify-center">{icons}</div>
-// </figure>
-//         <div className="card-body items-center">
-//           <h2 className="card-title font-merriWeather">{text}</h2>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Card;
-
 const Card = ({ text, icons }) => {
-  return <div className="animate-card">hello world</div>;
+  return (
+    <div className="" data-aos="zoom-in" data-aos-duration="1500">
+      <div className="card my-4 bg-base-100 hover:bg-text-Color hover:text-black hover:scale-110 transition hover:shadow-xl duration-1000 shadow-xl">
+        <figure className="min-w-64 ">
+          <div className="min-w-60 pt-6  flex justify-center">{icons}</div>
+        </figure>
+        <div className="card-body items-center">
+          <h2 className="card-title font-merriWeather">{text}</h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
